cc315c7bb1479c3f4cc677a3da9b91d4cc05868a
Mikołaj Jakubowski <gishelher@gmail.com>
2013-11-02 17:36:33 +0000

    fixed sbt config, added scalamodules

---
+++ project/Modules.scala
@@ -0,0 +1,77 @@
+import sbt._
+import Process._
+import Keys._
+
+object Modules extends Build {
+
+  val tests = Seq(
+    "org.scalatest" %% "scalatest" % "1.9.2" % "test",
+    "junit" % "junit" % "4.11" % "test",
+    "org.scalamock" %% "scalamock-scalatest-support" % "3.0.1" % "test",
+    "com.typesafe.akka" %% "akka-testkit"  % "2.2.3" % "test"
+  )
+
+  val akka = "com.typesafe.akka" %% "akka-actor"  % "2.2.3"
+
+  val osgi = Seq(
+    "org.osgi" % "org.osgi.core" % "4.2.0",
+    "org.osgi" % "org.osgi.compendium" % "4.2.0",
+    "com.weiglewilczek.slf4s" % "slf4s_2.9.1" % "1.0.7"
+  )
+
+  val typesafe = Seq(
+    Classpaths.typesafeReleases,
+    Classpaths.typesafeSnapshots,
+    "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/",
+    "Typesafe Snapshots" at "http://repo.typesafe.com/typesafe/snapshots/"
+  )
+
+  lazy val baseDeps = tests ++ osgi ++ Seq(
+    akka
+  )
+
+  val appName = "hot-actor-swap"
+  val appVersion = "0.1"
+
+  // Build definition
+  lazy val buildSettings = Seq(
+    organization := "com.virtuslab.has",
+    version := appVersion,
+    scalaVersion := "2.10.3"
+  )
+
+  // resolvers, dependencies and so on
+  lazy val defaultSettings = buildSettings ++ Seq(
+    resolvers ++= typesafe,
+    libraryDependencies ++= baseDeps
+  )
+
+  val alsoOnTests = "compile->compile;test->test"
+
+  lazy val scalaModules = sbt.Project(
+    id = appName + "-scalamodules",
+    base = file("scalamodules"),
+    settings = Project.defaultSettings ++ defaultSettings
+  )
+
+  lazy val akkaBundle = sbt.Project(
+    id = appName + "-akka",
+    base = file("bundles") / "akka",
+    settings = Project.defaultSettings ++ defaultSettings
+  ).aggregate(
+    scalaModules
+  ).dependsOn(
+    scalaModules
+  )
+  
+  lazy val root = sbt.Project(
+    id = appName,
+    base = file("."),
+    settings = Project.defaultSettings ++ defaultSettings
+  ).aggregate(
+    akkaBundle, scalaModules
+  ).dependsOn(
+    akkaBundle, scalaModules
+  )
+  
+}
